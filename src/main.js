a443996d3978542cf25d90b143ab2f7d20c05d81
feima1234 <1021261573@qq.com>
2023-06-28 20:24:29 +0800

    静态资源引入和插件安装

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import './assets/main.css'
+// 引入初始化的项目文件
+import '@/styles/common.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import Layout from '@/views/Layout/index.vue'
+import Login from '@/views/Login/index.vue'
+import Home from '@/views/Home/inex.vue'
+import Category from '@/views/Category/inex.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    { path:'/', component:Layout,
+    children:[
+      { path:'', component: Home},
+      { path:'category', component: Category}
+    ]},
+    { path:'/login', component:Login }
+  ]
 })
 
 export default router
